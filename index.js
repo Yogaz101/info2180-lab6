f8aeba17688c199ce9d36c449633b8c8a4277653
Jabari Charles <jabari_charles75@outlook.com>
2018-11-14 04:58:48 +0000

    third commit

--- index.js
+++ index.js
@@ -2,5 +2,22 @@ window.onload=function(){
     var button=document.getElementById("search");
     var text=document.getElementById("lookup");
     
-     
+    button.onclick=function(){
+        console.log('Search button Clicked')
+        var httpRequest=new XMLHttpRequest();
+        var url="request.php?q="+ text.value;
+        httpRequest.onreadystatechange=function(){
+            if(httpRequest.readyState===XMLHttpRequest.DONE){
+                if(httpRequest.status===200){
+                    var response=httpRequest.responseText;
+                    document.getElementById("result").innerHTML=response;
+                }
+                else{
+                    alert('There was a problem with the request.');
+                }
+            }
+        };
+        httpRequest.open('GET',url);
+        httpRequest.send();
+    };
 }
