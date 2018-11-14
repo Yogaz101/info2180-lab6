f637310fdeaaa28498bfc79840cc7bb48e82a93f
Jabari Charles <jabari_charles75@outlook.com>
2018-11-14 04:13:25 +0000

    second commit

--- index.js
+++ index.js
@@ -4,7 +4,20 @@ window.onload=function(){
     
     function alertTest(){
          button.addEventListener("click", function(){
-             alert("A statement of the exact meaning of a word, especially in a dictionary.");
+             var httpRequest = new XMLHttpRequest();
+var url= "request.php?q="+text.value;
+httpRequest.onreadystatechange = function(){
+if (httpRequest.readyState=== XMLHttpRequest.DONE){
+if (httpRequest.status===200){
+var i=httpRequest.responseText;
+document.getElementById("result").innerHTML=i;
+} else{
+alert("We cannot find your word");
+}
+}
+};
+httpRequest.open('GET', url);
+httpRequest.send();
          });
     }
     
