70c9f36ace53f7aabf9504e8af7a1891098b53b0
Jabari Charles <jabari_charles75@outlook.com>
2018-11-14 19:56:59 +0000

    third commit

--- index.js
+++ index.js
@@ -2,7 +2,14 @@ window.onload=function(){
     var button=document.getElementById("search");
     var text=document.getElementById("lookup");
     
-    function test(){
+    
+    function t(){
+        button.addEventListener("click",function(){
+            alert("A statement of the exact meaning of a word, especially in a dictionary.")
+        })
+    }
+    
+    /*function test(){
         button.addEventListener("click",function(){
         var httpRequest=new XMLHttpRequest();
         var url="request.php?q="+ text.value;
@@ -21,7 +28,7 @@ window.onload=function(){
         httpRequest.send();
             });
         
-    };
+    };*/
     
-    
+    t();
 }
